fc77ce9cae2e273aa94c1a2b60534f676b5e498e
Stijn Debrouwere <stijn@debrouwere.org>
2015-03-11 09:27:16 +0100

    Use werkzeug instead of flask for the oAuth single-serve. Fix #8.

--- googleanalytics/utils/server.py
+++ googleanalytics/utils/server.py
@@ -2,22 +2,22 @@
 
 def single_serve(message=None, port=5000):
     import logging
-    from flask import Flask, Response, request
-
-    app = Flask(__name__)
+    from werkzeug.wrappers import Request, Response
+    from werkzeug.serving import run_simple
 
     log = logging.getLogger('werkzeug')
     log.setLevel(logging.ERROR)
 
     captured = {}
 
-    @app.route('/')
-    def main():
+    def application(environ, start_response):
+        request = Request(environ)
         request.environ.get('werkzeug.server.shutdown')()
         captured.update(dict(request.args.items()))
         if message:
             print(message)
-        return Response(message, mimetype='text/plain')
+        response = Response(message, mimetype='text/plain')
+        return response(environ, start_response)
 
-    app.run(port=port)
+    run_simple('localhost', port, application)
     return captured
