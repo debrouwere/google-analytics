633ba35c87f62b70e0aae275519514632d33a777
Stijn Debrouwere <stijn@debrouwere.org>
2015-05-16 15:30:20 -0700

    Add version number to package.

--- googleanalytics/__init__.py
+++ googleanalytics/__init__.py
@@ -1,5 +1,9 @@
 # encoding: utf-8
 
+import pkg_resources
+
 from . import auth, commands, tests, utils, account, auth, blueprint, columns, errors, query, segments
 from .auth import authenticate, authorize, revoke
 from .blueprint import Blueprint
+
+__version__ = pkg_resources.get_distribution("googleanalytics").version
