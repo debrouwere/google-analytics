bc7c7260c814cd7bc9fa69ea2ef75e73c9ff7d7d
Stijn Debrouwere <stijn@debrouwere.org>
2015-05-22 12:52:06 +0200

    Add CLI version flag.

--- googleanalytics/commands/common.py
+++ googleanalytics/commands/common.py
@@ -6,13 +6,14 @@ import inspector
 import googleanalytics as ga
 
 
-@click.group()
+@click.group(invoke_without_command=True)
 @click.option('--identity')
 @click.option('--account')
 @click.option('--webproperty')
 @click.option('--profile')
+@click.option('--version', is_flag=True)
 @click.pass_context
-def cli(ctx, identity, account, webproperty, profile):
+def cli(ctx, identity, account, webproperty, profile, version):
     ctx.obj = ga.authenticate(
         identity=identity,
         account=account,
@@ -20,3 +21,6 @@ def cli(ctx, identity, account, webproperty, profile):
         profile=profile,
         interactive=True,
         save=True)
+
+    if version:
+        click.echo('googleanalytics {}'.format(ga.__version__))
